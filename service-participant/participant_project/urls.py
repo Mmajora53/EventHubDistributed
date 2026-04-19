bc41e5d5704846a628c91d8b26a850103ad6d716
Mmajora53 <maria.aydin.2004@gmail.com>
2026-04-19 16:59:26 +0200

    Màj des requirements.txt

--- service-participant/participant_project/urls.py
+++ service-participant/participant_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for eventhub_project project.
+URL configuration for participant_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
