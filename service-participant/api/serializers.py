ea033ad4abb1bb436dbaaa73aab285b34a2b113f
Mmajora53 <maria.aydin.2004@gmail.com>
2026-04-19 16:37:15 +0200

    Ajout du deuxième service

---
+++ service-participant/api/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from .models import Event, Participant, Registration, UserProfile
+from django.contrib.auth.models import User
+from datetime import date
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    avatar_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'avatar_url']
+
+    def get_avatar_url(self, obj):
+        profile, _ = UserProfile.objects.get_or_create(user=obj)
+        return profile.get_avatar_url()
+
+
+class EventSerializer(serializers.ModelSerializer):
+    members = MemberSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+        read_only_fields = ['creator', 'members']
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = '__all__'
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Registration
+        fields = '__all__'
+
+    def validate(self, data):
+        event = data.get('event')
+        participant = data.get('participant')
+        if Registration.objects.filter(event=event, participant=participant).exists():
+            raise serializers.ValidationError("This participant is already registered for this event.")
+        return data
+
+class EventSerializer(serializers.ModelSerializer):
+    members = MemberSerializer(many=True, read_only=True)
+    creator_username = serializers.CharField(source='creator.username', read_only=True)
+    creator_avatar = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+        read_only_fields = ['creator', 'members']
+
+    def get_creator_avatar(self, obj):
+        if not obj.creator:
+            return None
+        profile, _ = UserProfile.objects.get_or_create(user=obj.creator)
+        return profile.get_avatar_url()
+    
+    def validate_date(self, value):
+        if value < date.today():
+            raise serializers.ValidationError("You cannot create or edit an event for a past date.")
+        return value
