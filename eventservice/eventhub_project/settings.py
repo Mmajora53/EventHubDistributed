0441b5c6fb0d36368a33b4d9123aaa27695fb8d9
Clara AM <claraaitm@gmail.com>
2026-04-20 17:12:32 +0200

    update django eventservice of mysql

--- eventservice/eventhub_project/settings.py
+++ eventservice/eventhub_project/settings.py
@@ -82,10 +82,18 @@ WSGI_APPLICATION = 'eventhub_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eventhub_db',
+        'USER': 'root',
+        'PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD', 'test1234'),
+        'HOST': os.getenv('MYSQL_HOST', 'mysql'),
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
